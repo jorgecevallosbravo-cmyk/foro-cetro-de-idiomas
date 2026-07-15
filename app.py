8be2c8f2e023a390aa4ba5ac9b2c908a55e208d9
jorgecevallosbravo-cmyk <jorgecevallosbravo@gmail.com>
2026-07-14 21:55:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ elif database_url.startswith("postgresql://"):
 
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+    "pool_pre_ping": True,
+    "pool_recycle": 280,
+}
 
 db.init_app(app)
 
